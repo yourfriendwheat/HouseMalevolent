4eb4a4a62b0e22c40ce9492d0627c38a0e0d0fc4
Sze <sz982600@ucf.edu>
2025-02-11 17:59:16 -0500

    Updated NavMesh and Player&Enemy Movement

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] private Transform[] waypoints;
+    private int currentWaypointIndex = 0;
+    private UnityEngine.AI.NavMeshAgent navMeshAgent;
+
+    void Start()
+    {
+        navMeshAgent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        MoveToNextWaypoint();
+    }
+
+    void MoveToNextWaypoint()
+    {
+        if (waypoints.Length == 0) return;
+        navMeshAgent.SetDestination(waypoints[currentWaypointIndex].position);
+    }
+
+    void Update()
+    {
+        if (!navMeshAgent.pathPending && navMeshAgent.remainingDistance < 0.5f)
+        {
+            currentWaypointIndex = (currentWaypointIndex + 1) % waypoints.Length;
+            MoveToNextWaypoint();
+        }
+    }
+}
