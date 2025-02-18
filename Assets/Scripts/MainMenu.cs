2a8a0d04c3c543812fd55dcfe56efd464cb9fc2d
Sze <sz982600@ucf.edu>
2025-02-18 14:20:43 -0500

    Update MainMenu.cs

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.SceneManagement;
-using static UnityEditor.Timeline.TimelinePlaybackControls;
 
 public class MainMenu : MonoBehaviour
 {
