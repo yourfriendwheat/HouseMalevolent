5ecda0e60c6c2e970bf24be3dbc5182843c7f124
Sze <sz982600@ucf.edu>
2025-02-17 20:10:04 -0500

    updated MainMenu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using static UnityEditor.Timeline.TimelinePlaybackControls;
+
+public class MainMenu : MonoBehaviour
+{
+    bool settingMenu = false;
+    public GameObject buttons;
+    public GameObject SettingMenu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SettingMenu.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Experinment_codingInput");
+    }
+
+    public void OpenSetting()
+    {
+        settingMenu = true;
+        SettingMenu.SetActive(true);
+
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
